76887d874f0941212017e47951e4bcce0a11912d
Back <back4936nantawat@gmail.com>
2022-07-17 21:51:55 +0700

    move handle2

--- routes/line.js
+++ routes/line.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const config = require("../config/line");
-const handleEvent = require("../services/handle-event");
+const service = require("../services/handle-event");
 /* GET users listing. */
 router.post(
   "/callback",
   config.line.middleware(config.lineConfig),
   function (req, res, next) {
     // res.send("line callback4");
-    Promise.all(req.body.events.map(handleEvent))
+    Promise.all(req.body.events.map(service.handleEvent))
       .then((result) => res.json(result))
       .catch((err) => {
         console.error(err);
