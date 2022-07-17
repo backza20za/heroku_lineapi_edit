5689c0a68be41fa19b86d6e784e2b2cff320be7f
Back <back4936nantawat@gmail.com>
2022-07-17 22:52:32 +0700

    send sticker

--- services/message/sendText.js
+++ services/message/sendText.js
@@ -7,6 +7,12 @@ exports.sendText = (event) => {
     msg = { type: "text", text: "first promotion" };
   } else if (msgText === "555") {
     msg = { type: "text", text: "555" };
+  } else if (msgText === "love") {
+    msg = {
+      type: "sticker",
+      packageId: "789",
+      stickerId: "10856",
+    };
   } else {
     msg = { type: "text", text: "unknow promotion" };
   }
