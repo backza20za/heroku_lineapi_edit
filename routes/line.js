45f91baa1e0d080097dd2b8c26a9a2f07346f43f
Back <back4936nantawat@gmail.com>
2022-07-17 20:54:01 +0700

    connect line

--- routes/line.js
+++ routes/line.js
@@ -1,8 +1,8 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
 /* GET users listing. */
-router.get("/callback", function (req, res, next) {
+router.post("/callback", function (req, res, next) {
   res.send("line callback4");
 });
 
