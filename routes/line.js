aaffa356bd5e680854bd290cced5049a79fdc656
Back <back4936nantawat@gmail.com>
2022-07-17 14:51:03 +0700

    test edit2

--- routes/line.js
+++ routes/line.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get("/callback", function (req, res, next) {
-  res.send("line callback");
+  res.send("line callback1");
 });
 
 module.exports = router;
