a2765498361fa04e278643093eecb108d2bfe4ca
Back <back4936nantawat@gmail.com>
2022-07-17 15:18:10 +0700

    tset last

--- routes/line.js
+++ routes/line.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get("/callback", function (req, res, next) {
-  res.send("line callback3");
+  res.send("line callback4");
 });
 
 module.exports = router;
