c1f5e149abc9bc8cd6b2c39f99eee30930eb6587
Back <back4936nantawat@gmail.com>
2022-07-17 20:19:32 +0700

    commit ap new

--- app.js
+++ app.js
@@ -14,12 +14,13 @@ const app = express();
 app.use(logger("dev"));
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use("/", indexRouter);
 app.use("/line", lineRouter);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 module.exports = app;
