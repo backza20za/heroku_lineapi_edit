2c17b1439615e6cb591fe90d7277e6638f088abf
Back <back4936nantawat@gmail.com>
2022-07-24 22:57:01 +0700

    new commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/auth/callback", async (req, res, next) => {
+  const domain = "https://back-line-api.herokuapp.com/auth/callback";
+  return res.status(200).json({ message: "Login Success" });
 });
 
 module.exports = router;
