1805c7adcec5fd38a8df5b7f84b8dc8d7f993c64
Back <back4936nantawat@gmail.com>
2022-07-26 20:28:52 +0700

    new index

--- routes/index.js
+++ routes/index.js
@@ -14,18 +14,18 @@ router.get("/auth/callback", async (req, res, next) => {
   // return res.status(200).json({ message: "Login Success" });
 
   const params = new URLSearchParams({
-'grant_type':'authorization_code' 
-'code':req.query.code
-'redirect_uri':process.env.BASE_URL+"/auth/callback"
-'client_id':process.env.LOGIN_CLIENT_ID
-'client_secret':process.env.LOGIN_CLIENT_SECRET
+    grant_type: "authorization_code",
+    code: req.query.code,
+    redirect_uri: process.env.BASE_URL + "/auth/callback",
+    client_id: process.env.LOGIN_CLIENT_ID,
+    client_secret: process.env.LOGIN_CLIENT_SECRET,
   });
-  
+
   const response = await axios.post(
     "https://api.line.me/oauth2/v2.1/token",
     params,
     {
-      headers: { "Content-Type": "application/x-www-form-urlencoded"},
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
     }
   );
 });
