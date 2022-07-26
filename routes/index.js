65eb319f429e3c4632e0423c488786310610bc1c
Back <back4936nantawat@gmail.com>
2022-07-26 20:25:50 +0700

    new inde commit

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios").default;
+const jwt_decode = require("jwt-decode");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -9,8 +10,24 @@ router.get("/", function (req, res, next) {
 
 router.get("/auth/callback", async (req, res, next) => {
   // const domain = "https://back-line-api.herokuapp.com/auth/callback";
-  console.log(req.query.code);
-  return res.status(200).json({ message: "Login Success" });
+  // console.log(req.query.code);
+  // return res.status(200).json({ message: "Login Success" });
+
+  const params = new URLSearchParams({
+'grant_type':'authorization_code' 
+'code':req.query.code
+'redirect_uri':process.env.BASE_URL+"/auth/callback"
+'client_id':process.env.LOGIN_CLIENT_ID
+'client_secret':process.env.LOGIN_CLIENT_SECRET
+  });
+  
+  const response = await axios.post(
+    "https://api.line.me/oauth2/v2.1/token",
+    params,
+    {
+      headers: { "Content-Type": "application/x-www-form-urlencoded"},
+    }
+  );
 });
 
 module.exports = router;
